3ffec21f7bedc7eb34f90d112f28e3f1c9a9fe29
cht226 <cht226@lehigh.edu>
2023-02-24 10:35:41 -0500

    created server and client for socket

---
+++ connection.js
@@ -0,0 +1,10 @@
+const WebSocket = require('ws');
+const ws = new WebSocket("ws://localhost:8080");
+ws.addEventListener("", () =>{
+  console.log("We are connected");
+  ws.send("How are you?");
+});
+ 
+ws.addEventListener('message', function (event) {
+    console.log(event.data);
+});
