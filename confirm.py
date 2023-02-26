5e7100a2166b6cca2fa2c4089e812d68ffb49520
EthanLavi <esl225@lehigh.edu>
2023-02-26 16:25:36 -0500

    More fixes

--- confirm.py
+++ confirm.py
@@ -4,6 +4,8 @@ def querysniff(pkt):
     if "IP" in pkt:
         ip_src = pkt["IP"].src
         ip_dst = pkt["IP"].dst
+        if ip_src == "10.9.0.1" or ip_dst == "10.9.0.1":
+            return
         if ip_src[:2] == "10" and ip_dst[:2] == "10":
             print("ARP spoof was successful!")
             exit(0)
--- sniff.py
+++ sniff.py
@@ -7,13 +7,14 @@ def querysniff(pkt):
         if ip_src == "10.9.0.1" or ip_dst == "10.9.0.1":
             return
         if ip_src[:2] == "10" and ip_dst[:2] == "10":
+            print("\nStart Of Packet---")
             if "TCP" in pkt:
                 sport = pkt["TCP"].sport
                 dport = pkt["TCP"].dport
                 print("Source IP & Port:", str(ip_src) + ":" + str(sport), "\tDestination IP & Port:", str(ip_dst) + ":" + str(dport))
             else:
                 print("Source IP:", str(ip_src), "\tDestination IP:", str(ip_dst))
-            raw = packet.lastlayer()
+            raw = pkt.lastlayer()
             hexdump(raw)
 
 print("Searching for packets...")
