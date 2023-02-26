192ef6a40493c6e85fe2edad857965bd5d62a688
EthanLavi <esl225@lehigh.edu>
2023-02-26 16:09:09 -0500

    Fixed bug in programs

--- confirm.py
+++ confirm.py
@@ -4,8 +4,9 @@ def querysniff(pkt):
     if "IP" in pkt:
         ip_src = pkt["IP"].src
         ip_dst = pkt["IP"].dst
-        if ip_src[:2] == "10" and ip_dst[:2] == "10.9.0.6":
+        if ip_src[:2] == "10" and ip_dst[:2] == "10":
             print("ARP spoof was successful!")
             exit(0)
 
+print("Querying...")
 sniff(iface="eth0", prn=querysniff)
--- sniff.py
+++ sniff.py
@@ -4,7 +4,8 @@ def querysniff(pkt):
     if "IP" in pkt:
         ip_src = pkt["IP"].src
         ip_dst = pkt["IP"].dst
-        if ip_src[:2] == "10" and ip_dst[:2] == "10.9.0.6":
+        if ip_src[:2] == "10" and ip_dst[:2] == "10":
             ls(pkt)
 
+print("Searching for packets...")
 sniff(iface="eth0", prn=querysniff)
